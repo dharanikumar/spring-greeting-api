34b1ad6605ceed1e28e8cd3e3d60bcc54a6ca92a
dharanikumar <dharani939@gmail.com>
2020-08-26 01:22:55 +0530

    create service test file

---
+++ src/test/java/com/dhar/restapi/service/TodoServiceTest.java
@@ -0,0 +1,13 @@
+package com.dhar.restapi.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TodoServiceTest {
+    @InjectMocks
+    TodoService todoService;
+
+    //add tests for service methods
+}
